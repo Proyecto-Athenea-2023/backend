9a57442db3eb8e324c01584c64771d9efd7a505e
EngAndres <jerarquia04@gmail.com>
2023-05-05 07:29:57 -0500

    Adjust new application main module

---
+++ src/main/java/com/sergioarboleda/rent_car/RentCarApplication.java
@@ -0,0 +1,14 @@
+package com.sergioarboleda.rent_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class RentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarApplication.class, args);
+	}
+
+}
