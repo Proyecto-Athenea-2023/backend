fbb9e4797f3042d726cc5eaa2f04ee9faf3443ca
EngAndres <jerarquia04@gmail.com>
2023-05-11 08:39:20 -0500

    Add component of security using authentication based on GitHub OAuth2 client

---
+++ src/main/java/com/sergioarboleda/rent_car/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.sergioarboleda.rent_car.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User userInfo){
+        return Collections.singletonMap("name", userInfo.getAttribute("name"));
+    }
+}
