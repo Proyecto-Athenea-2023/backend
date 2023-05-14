38bc3649b49e4dd450d87bc090dba09e305361c7
EngAndres <jerarquia04@gmail.com>
2023-05-14 09:34:24 -0500

    Apply improvements to Clients module

--- src/main/resources/static/js/clients.js
+++ src/main/resources/static/js/clients.js
@@ -1,3 +1,40 @@
+/*
+* <p>Copyright: Copyright (c) 2023</p>
+*
+* <h3>License</h3>
+*
+* Copyright (c) 2023 by Carlos Andres Sierra Virguez. <br>
+* All rights reserved. <br>
+*
+* <p>Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*   <ul>
+*       <li> Redistributions of source code must retain the above copyright notice,
+*            this list of conditions and the following disclaimer.
+*       <li> Redistributions in binary form must reproduce the above copyright notice,
+*            this list of conditions and the following disclaimer in the documentation
+*            and/or other materials provided with the distribution.
+*       <li> Neither the name of the copyright owners, their employers, nor the
+*            names of its contributors may be used to endorse or promote products
+*            derived from this software without specific prior written permission.
+*   </ul>
+* <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+*    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+*    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*    DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNERS OR CONTRIBUTORS BE
+*    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+*    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+*    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+*    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*    POSSIBILITY OF SUCH DAMAGE.
+*
+* @author <A HREF="https://www.linkedin.com/in/casierrav/"> Carlos Andres Sierra </A>
+* (E-mail: <A HREF="mailto:carlos.andres.sierra.v@gmail.com">carlos.andres.sierra.v@gmail.com</A> )
+* @version 1.0
+*/
+
 // Variables Globales
 let URL_BASE = "http://localhost:8080"
 let client = null;
@@ -61,8 +98,8 @@ function updateClientsTable(clients){
         data += "<td>" + clients[i].phones + "</td>";
         data += "<td>" + clients[i].email + "</td>";
         data += "<td>" + clients[i].nickname + "</td>";
-        data += "<td>" + "<span onclick=\"openModal("+ clients[i].idClient +")\">Actualizar</span>" + "</td>";
-        data += "<td>" + "<span onclick=\"deleteClient("+ clients[i].idClient +")\">Eliminar</span>" + "</td>";
+        data += "<td>" + "<span onclick=\"openModal("+ clients[i].idClient +")\"><img src=\"media/update.png\" alt=\"A\" width=\"25px\" height=\"25px\"></span>" + "</td>";
+        data += "<td>" + "<span onclick=\"deleteClient("+ clients[i].idClient +")\"><img src=\"media/delete.png\" alt=\"E\" width=\"25px\" height=\"25px\"></span>" + "</td>";
         data += "</tr>";
     }
     $("#tblClients > tbody:last-child").append(data);
@@ -122,6 +159,8 @@ function insertClient(){
         contentType: "application/json;charset=UTF-8"
     })
     .done(function(response){
+        console.log(body);
+        console.log(response);
         alert("Cliente agregado.");
         getAllClients();
     })
